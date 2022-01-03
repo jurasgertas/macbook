2b8a6d1cb7dccbf9ca6bd85e7ce69924955a5c49
jurasgertas <75979854+jurasgertas@users.noreply.github.com>
2022-01-03 07:53:46 +0100

    Update newmac.sh

--- newmac.sh
+++ newmac.sh
@@ -1,1 +1,214 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# 
+
+echo "Starting"
+
+# Check for Homebrew, install if we don't have it
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Update homebrew recipes
+brew update
+
+#define packages to be installed
+PACKAGES=(
+    git
+    wget
+    mas
+    glances
+    cask
+    telnet
+    nmap
+    tree
+    arp-scan
+    hudochenkov/sshpass/sshpass
+    duck
+    cyberduck
+    teamviewer
+    termius
+    xclip
+    dockutil
+)
+
+echo "Installing packages..."
+brew install ${PACKAGES[@]}
+
+echo "link termius"
+brew link termius
+
+echo "Cleaning up..."
+brew cleanup
+
+#define cask packages to be installed
+CASKS=(
+    itsycal
+    iterm2
+    enpass
+    tuxera-ntfs
+    firefox
+    balenaetcher
+    drawio
+    hush
+    cheatsheet
+    spotify
+    alfred
+    onlyoffice
+    mathpix-snipping-tool
+    sensei
+    pwgen
+    sublime-text
+    vlc
+    tunnelblick
+    cyberduck
+    teamviewer
+    termius
+    airflow
+    deepl
+)
+
+echo "Installing cask apps..."
+brew install --cask ${CASKS[@]}
+
+echo "installing anaconda"
+wget https://repo.anaconda.com/archive/Anaconda3-2021.05-MacOSX-x86_64.pkg
+PKG=$(find *.pkg)
+sudo installer -package $PKG -target /
+rm $PKG
+
+echo "mac configuration"
+echo "configuring colours in shell"
+
+echo 'export CLICOLOR=1' | sudo tee -a ~/.zshrc
+echo 'export LSCOLORS=ExFxBxDxCxegedabagacad' | sudo tee -a ~/.zshrc
+echo 'alias ls='ls -GFh'' | sudo tee -a ~/.zshrc
+echo 'export CLICOLOR=1' | sudo tee -a ~/.bash_profile
+echo 'export LSCOLORS=ExFxBxDxCxegedabagacad' | sudo tee -a ~/.bash_profile
+echo 'alias ls='ls -GFh'' | sudo tee -a ~/.bash_profile
+
+echo "removing and adding icons over dockutil"
+
+dockutil --remove 'Contacts'
+dockutil --remove 'Maps'
+dockutil --remove 'FaceTime'
+dockutil --remove 'Reminders'
+dockutil --remove 'TV'
+dockutil --remove 'Music'
+dockutil --remove 'Podcasts'
+dockutil --add /Applications/Termius.app
+dockutil --add /Applications/iTerm.app
+dockutil --add '/Applications/PyCharm CE.app'
+dockutil --add /Applications/Anaconda-Navigator.app
+dockutil --add /Applications/Enpass.app
+dockutil --add '/Applications/Sublime Text.app'
+dockutil --add '/Applications/Cyberduck.app'
+dockutil --add '/Applications/Spotify.app'
+
+echo "changing hostname"
+
+echo "$(tput setaf 1)$(tput setab 7) \
+Type your hostname (rmbp): \
+$(tput sgr 0)"
+read compname
+sudo scutil --set ComputerName $compname
+sudo scutil --set LocalHostName $compname
+sudo scutil --set HostName $compname
+
+echo "sign into appstore"
+
+echo "$(tput setaf 1)$(tput setab 7) \
+Login to app store. Press Enter when done: \
+$(tput sgr 0)"
+
+#Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Remove duplicates in the “Open With” menu (also see `lscleanup` alias)
+/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
+
+# Disable automatic capitalization as it’s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+# Disable smart dashes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Disable automatic period substitution as it’s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+# Disable smart quotes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+# Set language and text formats
+# Note: if you’re in the US, replace `EUR` with `USD`, `Centimeters` with
+# `Inches`, `en_GB` with `en_US`, and `true` with `false`.
+defaults write NSGlobalDomain AppleLanguages -array "en" "cs"
+defaults write NSGlobalDomain AppleLocale -string "en_US@currency=EUR"
+defaults write NSGlobalDomain AppleMeasurementUnits -string "Centimeters"
+defaults write NSGlobalDomain AppleMetricUnits -bool true
+
+# Set the timezone; see `sudo systemsetup -listtimezones` for other values
+sudo systemsetup -settimezone "Europe/Prague" > /dev/null
+
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Keep folders on top when sorting by name
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Disable disk image verification
+defaults write com.apple.frameworks.diskimages skip-verify -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+
+# Automatically open a new Finder window when a volume is mounted
+defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `glyv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+# Disable the warning before emptying the Trash
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+# Enable AirDrop over Ethernet and on unsupported Macs running Lion
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
+
+# Show the ~/Library folder
+chflags nohidden ~/Library && xattr -d com.apple.FinderInfo ~/Library
+
+# Show the /Volumes folder
+sudo chflags nohidden /Volumes
+
+# Enable highlight hover effect for the grid view of a stack (Dock)
+defaults write com.apple.dock mouse-over-hilite-stack -bool true
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+# Hide & Show it fast
+defaults write com.apple.dock autohide-time-modifier -float 0; killall Dock
+
+echo "complete, please reboot"
